c52c2f631a9a6d377b5ffb6d2098bc36c7ab0ea8
Neha kiran <nehakiran@Nehas-MacBook-Air.local>
2024-12-11 21:28:07 +0530

    part-1

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1",{id:"heading", xyz:"xyz"},"Hello World from react!");
+
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+
+    root.render(heading);
